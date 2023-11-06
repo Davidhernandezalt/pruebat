71eaa5c8da7aa0c0ba57eb11298f3061ca18fee9
Davidhernandezalt <146228222+Davidhernandezalt@users.noreply.github.com>
2023-11-05 20:13:25 -0600

    version test

--- wsgi.py
+++ wsgi.py
@@ -1,20 +1,6 @@
-from libretranslate import main
+from libretranslate import create_app
 
+app = create_app()
 
-def app(*args, **kwargs):
-    import sys
-    sys.argv = ['--wsgi']
-    for k in kwargs:
-        ck = k.replace("_", "-")
-        if isinstance(kwargs[k], bool) and kwargs[k]:
-            sys.argv.append("--" + ck)
-        else:
-            sys.argv.append("--" + ck)
-            sys.argv.append(kwargs[k])
-
-    instance = main()
-
-    if len(kwargs) == 0:
-        return instance(*args, **kwargs)
-    else:
-        return instance
+if __name__ == "__main__":
+    app.run()
